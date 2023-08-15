73dff249f5bc0205af38faecf873ea72da5a717b
Mukund-Thirugnanasambanthar <116257453+Mukund-Thirugnanasambanthar@users.noreply.github.com>
2023-08-15 08:44:30 +0200

    Add files via upload

---
+++ life_predictor.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import keras
+import numpy as np
+
+def life(data,case):
+    max_stress=[]
+    for i in range(len(data)):
+        if case[i] == 'Case_1':
+            name = 'Case1.json'
+        elif case[i] == 'Case_2':
+            name = 'Case2.json'
+        elif case[i] == 'Case_3':
+            name = 'Case3.json'
+        elif case[i] == 'Case_4':
+            name = 'Case4.json'
+        elif case[i] == 'Case_5':
+            name = 'Case5.json'
+        load_model=tf.keras.saving.load_model(name)
+        pred=load_model.predict(accel[i])
+        pred=np.array(pred)
+        pred=np.reshape(pred,(30255,1))
+        max_stress.append(max(pred))
+    return max_stress
