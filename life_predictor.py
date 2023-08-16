9958a7d4e94f52145fe58d249e5b72e9d9c8b130
Mukund-Thirugnanasambanthar <116257453+Mukund-Thirugnanasambanthar@users.noreply.github.com>
2023-08-16 15:11:00 +0200

    Update life_predictor.py

--- life_predictor.py
+++ life_predictor.py
@@ -3,21 +3,21 @@ import keras
 import numpy as np
 
 def life(data,case):
-    #max_stress=[]
-    #for i in range(len(data)):
-     #   if case[i] == 'Case_1':
-      #      name = 'Case1.json'
-       # elif case[i] == 'Case_2':
-        #    name = 'Case2.json'
-        #elif case[i] == 'Case_3':
-         #   name = 'Case3.json'
-        #elif case[i] == 'Case_4':
-         #   name = 'Case4.json'
-        #elif case[i] == 'Case_5':
-         #   name = 'Case5.json'
-        #load_model=tf.keras.saving.load_model(name)
-        #pred=load_model.predict(data[i])
-        #pred=np.array(pred)
-        #pred=np.reshape(pred,(30255,1))
-        #max_stress.append(max(pred))
-    return len(data),len(case)
+    max_stress=[]
+    for i in range(len(data)):
+        if case[i] == 'Case_1':
+            name = 'Case1.json'
+        elif case[i] == 'Case_2':
+            name = 'Case2.json'
+        elif case[i] == 'Case_3':
+            name = 'Case3.json'
+        elif case[i] == 'Case_4':
+            name = 'Case4.json'
+        elif case[i] == 'Case_5':
+            name = 'Case5.json'
+        load_model=tf.keras.saving.load_model('C:/Users/Mukund/Documents/Digital_Twin/Voxel/'+str(name))
+        pred=load_model.predict([abs(data[i])])
+        pred=np.array(pred)
+        pred=np.reshape(pred,(30255,1))
+        max_stress.append(max(pred))
+    return max_stress
